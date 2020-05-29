3b74295332dc79cbac9c75d32a3770b8295c11c6
Agustin Díaz <agustin.ramiro.diaz@gmail.com>
2020-05-29 20:07:10 -0300

    Primera versión ListSeq
    Co-authored-by: SantIgD <demagistris.santiago@gmail.com>

--- ListSeq.hs
+++ ListSeq.hs
@@ -1,40 +1,24 @@
 module ListSeq where
 
 import Seq
-
-data TreeView a = E | EL a | Node [a] [a]
-
+--mapS , appendS, reduceS, scanS {Hacer nosotros}
 instance Seq [] where
-    emptyS = vacia
-    singletonS = singular
-    lengthS = longitud
-    nthS = enesimo
+    emptyS = []
+    singletonS x = [x]
+    lengthS = length
+    nthS l n = l !! n
     tabulateS = tabular
     mapS = mapear
-    filterS = filtrar
+    filterS = filter
     appendS = concatenarLista
     takeS lista n = take n lista
     dropS lista n = drop n lista
-    showtS        = mostrarArbol    
+    showtS = mostrarArbol
     showlS = mostrarLista
-    joinS  = aplanar
-    -- reduceS    :: (a -> a -> a) -> a -> s a -> a
+    joinS = concat
+    reduceS = reducir
     scanS = escanear
-    fromList = desdeLista
-
-vacia :: [a]
-vacia = []
-
-singular :: a -> [a]
-singular x = [x]
-
-longitud :: [a] -> Int
-longitud [] = 0
-longitud (h: t) = 1 + longitud t
-
-enesimo :: [a] -> Int -> a
-enesimo (h: t) 0 = h
-enesimo (h: t) n = enesimo t (n-1)
+    fromList = id
 
 tabular :: (Int -> a) -> Int -> [a]
 tabular f 0 = []
@@ -48,44 +32,33 @@ mapear :: (a -> b) -> [a] -> [b]
 mapear f [] = []
 mapear f (h: t) = (f h): (mapear f t)
 
-filtrar :: (a -> Bool) -> [a] -> [a] 
-filtrar p [] = []
-filtrar p (h: t)
-    | p h = h: (filtrar p t)
-    | otherwise = filtrar p t
-
-
 concatenarLista :: [a] -> [a] -> [a]
-concatenarLista xs ys = xs ++ ys
-
+concatenarLista [] ys = ys
+concatenarLista (h: t) ys = h : (concatenarLista t ys)
 
 mostrarArbol :: [a] -> TreeView a [a]
-mostrarArbol []     = E
-mostrarArbol [x]    = EL x
-mostrarArbol (x:xs) = Node (take seq  (l / 2)) Node (drop seq  (l / 2))
-                      where
-                           l = longitud (x:xs)
-
-
-aplanar :: [[a]] -> [a]
-aplanar [] = []
-aplanar [[x]] = [x]
-aplanar (xs : xss) = appendS xs (aplanar xss)
-
+mostrarArbol []     = EMPTY
+mostrarArbol [x]    = ELT x
+mostrarArbol xs     = NODE  left  right
+                        where
+                            mitad = div (length xs) 2
+                            (left, right) = splitAt mitad xs
 
 mostrarLista :: [a] -> ListView a [a]
 mostrarLista [] = NIL
 mostrarLista (h: t) = CONS h t
 
+aArbol :: (a -> a -> a)-> [a] -> a
+aArbol oplus [x] = x 
+aArbol oplus sequencia = oplus (aArbol oplus left) (aArbol oplus right)
+    where 
+        n = length sequencia
+        m = 2 ^ (floor $ (logBase 2 (fromIntegral (n - 1))))
+        (left, right) = splitAt m sequencia
 
-
-
-
-
-
-
-
-
+reducir :: (a -> a -> a) -> a -> [a] -> a
+reducir oplus neutro [] = neutro
+reducir oplus neutro sequencia = oplus neutro (aArbol oplus sequencia)
 
 escanear :: (a -> a -> a) -> a -> [a] -> ([a], a)
 escanear oplus n xs = escanear' oplus xs n
@@ -101,7 +74,17 @@ escanear' oplus (h: t) rs =
 
 
 
+[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+[1, 2, 3, 4, 5, 6, 7, 8] 
+[1, 2, 3, 4]          [5, 6, 7, 8] 
+([1, 2] f [3, 4]) f ([5, 6] f [7, 8]) f ((9 f 10) f 11)
 
 
-desdeLista :: [a] -> [a]
-desdeLista xs = xs
+
+
+
+         [9, 10, 11]
+
+     [9, 10]      [11]
+
+   (9 oplus 10)  oplus 11
